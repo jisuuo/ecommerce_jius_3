e9c2f596d33cc350394ddf5d4fd9ab0125f0410e
JISUOGIT <jisu2298@gmail.com>
2023-12-17 12:16:12 +0900

    User 패스 암호화

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,5 +1,6 @@
-import { Column, Entity } from 'typeorm';
+import { BeforeInsert, Column, Entity } from 'typeorm';
 import { BaseEntity } from '../../common/base.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Entity()
 export class User extends BaseEntity{
@@ -11,4 +12,10 @@ export class User extends BaseEntity{
 
   @Column()
   public password: string;
+
+  @BeforeInsert()
+  async hashPassword() : Promise<void> {
+    const saltValue = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, saltValue);
+  }
 }
